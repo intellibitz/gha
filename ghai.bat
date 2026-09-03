e72000d1e79e5d3eb1d06b50df9b940f73532f92
Muthu Ramadoss <muthu.ramadoss@gmail.com>
2026-09-03 22:46:28 +0530

    docs: update documentation and project wiki (#15)

--- ghai.bat
+++ ghai.bat
@@ -1,2 +1,4 @@
 @echo off
+rem 🤖 ghai - Autonomous AI Workflow Launcher
+rem Convenient shortcut wrapper for: .\gradlew.bat --init-script init/gha.init.gradle.kts ghai
 .\gradlew.bat --init-script init/gha.init.gradle.kts ghai %*
