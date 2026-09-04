8f96bfb81a7569c00664c8e14f8ee5affb37e63e
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-05 03:10:01 +0530

    status report and check orchestration

--- init/gha.init.gradle.kts
+++ init/gha.init.gradle.kts
@@ -7,7 +7,7 @@ initscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("cc.thevar.gha:gha:0.1.33-SNAPSHOT")
+        classpath("cc.thevar.gha:gha:0.1.35-SNAPSHOT")
     }
 }
 
