f63dbc6c304572a6b3e4cfc19e206e12b8725071
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-05 01:11:06 +0530

    feat: update hai and related components

--- init/gha.init.gradle.kts
+++ init/gha.init.gradle.kts
@@ -7,7 +7,7 @@ initscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("cc.thevar.gha:gha:0.1.16-SNAPSHOT")
+        classpath("cc.thevar.gha:gha:0.1.17-SNAPSHOT")
     }
 }
 
