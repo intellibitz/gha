d9a76885539d0e0bd4ca29e4008edb7e5a67db54
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-06 04:15:47 +0530

    bump: version 0.1.70

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use gemi::{GemiEngine, GemiServer, ModelManager};
 use gmcp::{GmcpClient, GmcpServer};
 use sandbox::SandboxManager;
 
-const GHA_VERSION: &str = "0.1.69";
+const GHA_VERSION: &str = "0.1.70";
 
 fn get_home_dir() -> PathBuf {
     env::var_os("HOME")
