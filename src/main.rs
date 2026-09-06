19141e83b8c33c27945a919f06a3042feb0eb667
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-06 12:26:07 +0530

    chore: sync version v0.1.79

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use gemi::{GemiEngine, GemiServer, ModelManager};
 use gmcp::{GmcpClient, GmcpServer};
 use sandbox::SandboxManager;
 
-const GHA_VERSION: &str = "0.1.78";
+const GHA_VERSION: &str = "0.1.79";
 
 fn get_home_dir() -> PathBuf {
     env::var_os("HOME")
