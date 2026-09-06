fc8d8e4e9b425bc030a8be20b07f53d75af55d9e
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-06 17:30:00 +0530

    🚀 Purify Root: Remove obsolete ghai/ghai.bat launchers. Native binary is the only way.

--- ghai.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "GLOBAL_GHA_BIN=%USERPROFILE%\.gha\bin\ghai.exe"
-if exist "%GLOBAL_GHA_BIN%" (
-    "%GLOBAL_GHA_BIN%" %*
-) else if exist "%~dp0target\release\gha.exe" (
-    "%~dp0target\release\gha.exe" %*
-) else (
-    cargo run --release -- %*
-)
