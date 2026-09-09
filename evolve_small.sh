000a337de5f14892d9a52d3b9a2f29ca44c58559
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-10 04:27:59 +0530

    chore: enforce workspace purity by moving pollutants to testspace and increment version to v0.1.2022581

--- evolve_small.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-for i in {1..5}
-do
-   echo "Cycle $i starting..."
-   ./target/release/gha advance
-done
--- mission.rs
+++
@@ -1,5 +0,0 @@
-'pub struct MissionControl {
-    pub id: String,
-    pub status: String,
-    pub telemetry: Vec<f64>,
-}'
