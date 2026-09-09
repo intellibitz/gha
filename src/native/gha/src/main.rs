920a5df717cb0fa7f23da78fec679deee9921d98
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-09 22:15:08 +0530

    release: v0.1.327 compliance sync

--- src/native/gha/src/main.rs
+++ src/native/gha/src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::io::{self, BufRead, Write};
 use std::path::{Path, PathBuf};
 
-const GHA_VERSION: &str = "0.1.325";
+const GHA_VERSION: &str = "0.1.327";
 
 fn get_home_dir() -> PathBuf {
     env::var_os("HOME")
