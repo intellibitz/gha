2116c521a8fa7321a63f6bf1b9458480008e0d58
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-10 13:38:12 +0530

    fix: synchronize all version constants across launcher, engine, and documentation to v0.1.2022600

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ use rustyline::hint::Hinter;
 use rustyline::validate::Validator;
 use rustyline::{Context, Helper};
 
-pub const GHA_VERSION: &str = "0.1.2022599";
+pub const GHA_VERSION: &str = "0.1.2022600";
 
 // ANSI Formatting Codes
 const COLOR_CYAN: &str = "\x1b[1;36m";
